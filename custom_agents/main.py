072a2514d5e7b51ab31fc5d0e080f6fd621b8de8
Ronald Foster <vendorservices@yumyumco.app>
2025-05-26 10:16:36 -0400

    Update main.py

--- custom_agents/main.py
+++ custom_agents/main.py
@@ -1,15 +1,15 @@
-from dotenv import load_dotenv
+import random
 import os
 import sys
 import uuid
 import asyncio
 import json
 import logging
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
-
+from dotenv import load_dotenv
 from custom_agents.team_builder import build_agents
 from custom_agents.agents.orchestrator import ChiefEditorAgent
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
@@ -34,16 +34,32 @@ def open_task(filepath):
 
 
 async def main():
-
     current_dir = os.path.dirname(os.path.abspath(__file__))
+
     task_path = os.path.join(current_dir, 'task.json')
+    author_path = os.path.join(current_dir, 'config/authors.json')
+
+    with open(author_path, 'r') as file:
+        authors_data = json.load(file)
+    author = random.choice(list(authors_data))
+
     task = open_task(task_path)
-    logging.info(f"Loaded task: {task}")
+    tone = author.get("tone")
+
+    task["query"] = f"{task.get('query', '')} {author.get('prompt_injection', '')}".strip()
+    task["guidelines"] = author.get("guidelines")
+    task["author"] = author.get("name")
+    task["avatar"] = author.get("avatar")
+    task["bio"] = author.get("bio")
+    task["tone"] = tone
+   # task["report_type"]
 
     agents = build_agents(task)
-    logging.info(f"Agents built for team: {task.get('team')} using model: {task.get('model')}")
 
-    chief_editor = ChiefEditorAgent(agents, task)
+    logging.info(
+        f"Agents built for team: {task.get('team')} with tone: {author.get("tone")}  using model: {task.get('model')} and Author:  {author.get('name')}")
+
+    chief_editor = ChiefEditorAgent(agents, task, tone)
     research_report = await chief_editor.run_research_task(task_id=uuid.uuid4())
 
     return research_report
